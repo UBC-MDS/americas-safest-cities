27c7a4585a1ba93ebe380ece2e004b072b56373f
Talha Siddiqui <talhaadnan100@gmail.com>
2019-01-18 12:02:49 -0800

    Basic functionality implemented

--- app.R
+++ app.R
@@ -6,32 +6,87 @@
 library(shiny)
 library(tidyverse)
 library(leaflet)
+library(shinythemes)
 
+crime <- tibble(crime_numbers = c("violent_crime","homs_sum","rape_sum","rob_sum","agg_ass_sum"),
+                crime_type = c("All","Homicide","Rape","Robbery","Aggravated Assault"))
 
+raw_data <- read_csv('data/data_clean.csv')
+
+data <- raw_data %>% 
+    gather(crime_numbers, Numbers, 7:11) %>% 
+    select(year, city, state, lat, long,
+           total_pop, crime_numbers, Numbers) %>% 
+    inner_join(crime) %>% 
+    mutate(Rates = 100000 * Numbers / total_pop)
 
 #########################
 # Define User Interface #
 #########################
 
 ui <- fluidPage(
-
+    
     # Application title
     titlePanel("America's Safest Cities"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+    
+    # Application theme
+    theme = shinytheme("readable"),
+    
+    
+    tabsetPanel(
+        
+        # Interactive map
+        tabPanel("Interactive Map",
+                 fluidRow(
+                     sidebarPanel(
+                         position = "right",
+                         width = 3,
+                         helpText("Click on the bubble to view data"),
+                         selectInput("yearInput", "Year",
+                                     choices = c(1975:2015), selected = 2015),
+                         selectInput("crimeInput", "Type of Violent crime",
+                                     choices = c("All","Homicide", "Rape","Robbery","Aggravated Assault"))
+                         #,radioButtons("radioInput", "", choices = c("Numbers", "Rates"))
+                         
+                     ),
+                     mainPanel(
+                         leafletOutput("map", width=800, height=600)
+                     )
+                 )
+                 
         ),
+        
+        # Data tab
+        tabPanel("Trend Chart",
+                 fluidRow(
+                     sidebarLayout(
+                         sidebarPanel(
+                             position = "right",
+                             width = 3,
+                             selectInput("durationInput", "Years",
+                                         choices = c(2,5,10,20,40), selected = 5),
+                             selectInput("crimeTrendInput", "Type of Violent crime",
+                                         choices = c("All","Homicide", "Rape","Robbery","Aggravated Assault")),
+                             selectInput("city1Input", "Selected Cities",
+                                         choices = data$city),
+                             selectInput("city2Input", "",
+                                         choices = data$city),
+                             selectInput("city3Input", "",
+                                         choices = data$city),
+                             selectInput("city4Input", "",
+                                         choices = data$city),
+                             selectInput("city5Input", "",
+                                         choices = data$city)
+                         ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+                         # Show a plot of the generated distribution
+                         mainPanel(
+                             plotOutput("trendPlot")
+                         )
+                     )
+                 )
         )
+        
     )
 )
 
@@ -40,14 +95,52 @@ ui <- fluidPage(
 #######################
 
 server <- function(input, output) {
+    
+    output$map <- renderLeaflet({
+        
+        # Leaflet dataframe
+        leaf_data <- data %>% 
+            arrange(year) %>% 
+            group_by(city, crime_type) %>% 
+            filter(year == input$yearInput, 
+                   crime_type == input$crimeInput) %>% 
+            filter(city != "National")
+        
+        
+        leaflet(data = leaf_data) %>%
+            addProviderTiles(providers$Stamen.TonerLite,
+                             options = providerTileOptions(noWrap = TRUE)
+            ) %>%
+            setView(lng = -93.85, lat = 37.45, zoom = 4) %>%
+            addTiles() %>% 
+            addCircleMarkers(
+                ~long, ~lat, 
+                radius =~(Numbers * 30/max(Numbers)), 
+                popup = ~paste(
+                    "</br><b>Crime:</b>",crime_type,
+                    "</br><b>City:</b>",city,
+                    "</br><b>Year:</b>",year,
+                    "</br><b>Total:</b>",round(Numbers)
+                ),
+                fillOpacity = 0.5
+            )
+    })
+    
+    output$trendPlot <- renderPlot({
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        selected_cities <- c(input$city1Input, input$city2Input, input$city3Input,
+                             input$city4Input, input$city5Input)
+        duration <- as.numeric(input$durationInput)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        data %>%
+            arrange(year) %>%
+            group_by(crime_type) %>%
+            filter(city == selected_cities,
+                   year >= (2015-duration),
+                   crime_type == input$crimeTrendInput) %>%
+            ggplot(aes(x = year, y = Rates, colour = city)) +
+            geom_line(size = 2)
+
     })
 }
 
