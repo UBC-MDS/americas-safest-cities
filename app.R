2021ac0f33ab967ac7eddcad35c3443ceff722cd
Talha Siddiqui <talhaadnan100@gmail.com>
2019-01-26 14:16:14 -0800

    App updates deployed and Readme editted

---
+++ app.R
@@ -0,0 +1,239 @@
+#
+# America's Most Dangerous Cities
+# Authors: Mohamad Makkaoui, Talha A. Siddiqui
+#
+
+library(shiny)
+library(tidyverse)
+library(leaflet)
+library(shinythemes)
+library(RColorBrewer)
+library(plotly)
+library(shinyWidgets)
+
+theme_set(theme_light())
+
+############################
+# Data Import & Processing #
+############################
+
+crime <-
+  tibble(
+    crime_numbers = c(
+      "violent_crime",
+      "homs_sum",
+      "rape_sum",
+      "rob_sum",
+      "agg_ass_sum"
+    ),
+    crime_type = c("All", "Homicide", "Rape", "Robbery", "Aggravated Assault")
+  )
+
+raw_data <- read_csv('data/data_cleaned.csv')
+
+data <- raw_data %>%
+  gather(crime_numbers, Numbers, 7:11) %>%
+  select(year, city, state, lat, long,
+         total_pop, crime_numbers, Numbers) %>%
+  inner_join(crime) %>%
+  mutate(Rates = 100000 * Numbers / total_pop)
+
+#########################
+# Define User Interface #
+#########################
+
+ui <- fluidPage(
+  #Application title
+  titlePanel("America's Most Dangerous Cities"),
+  
+  #Application theme
+  theme = shinytheme("readable"),
+  
+  
+  tabsetPanel(
+    # Interactive map
+    tabPanel("Interactive Map",
+             fluidRow(
+               sidebarPanel(
+                 width = 3,
+                 helpText("Click on a city's bubble to view detials"),
+                 # sliderInput(
+                 #   "yearInput",
+                 #   "Year",
+                 #   min = 1975,
+                 #   max = 2015,
+                 #   value = 2015,
+                 #   sep = ""
+                 # ),
+                 knobInput(
+                   inputId = "yearInput",
+                   label = "Year",
+                   value = 2015,
+                   min = 1975,
+                   max = 2015,
+                   displayPrevious = TRUE, 
+                   lineCap = "round",
+                   fgColor = '#F8766D',
+                   inputColor = '#F8766D',
+                   width = '95%', 
+                   height = '95%'
+                 ),
+                 selectInput(
+                   "crimeInput",
+                   "Type of Violent crime",
+                   choices = c("All", "Homicide", "Rape", "Robbery", "Aggravated Assault")
+                 ),
+                 helpText("Worst 5 Cities Ranked"),
+                 # Calling panel plot
+                 plotOutput("panelPlot")
+               ),
+               mainPanel(leafletOutput(
+                 "map", width = 1000, height = 600
+               ))
+             )),
+    
+    # Data tab
+    tabPanel("Trend Chart",
+             fluidRow(
+               sidebarLayout(
+                 sidebarPanel(
+                   position = "right",
+                   width = 3,
+                   checkboxGroupButtons(
+                     "durationInput",
+                     label = "No. of Years before 2015",
+                     choices = c(2, 5, 10, 20, 40),
+                     justified = TRUE,
+                     status = "info",
+                     selected = 20
+                   ),
+                   
+                   selectInput(
+                     "crimeTrendInput",
+                     "Type of Violent crime",
+                     choices = c("All", "Homicide", "Rape", "Robbery", "Aggravated Assault")
+                   ),
+                   pickerInput(
+                     inputId = "cityInput",
+                     label = "Select up to 5 cities to compare",
+                     choices = unique(data$city),
+                     multiple = TRUE,
+                     options = list('actions-box' = TRUE, "max-options" = 5),
+                     selected = c("Chicago", "Los Angeles")
+                   )
+                 ),
+                 
+                 # Show a plot of the generated distribution
+                 mainPanel(plotlyOutput("trendPlot"))
+               )
+             ))
+    
+  )
+)
+
+#######################
+# Define Server Logic #
+#######################
+
+server <- function(input, output) {
+  output$map <- renderLeaflet({
+    # Leaflet dataframe
+    leaf_data <- data %>%
+      filter(year == input$yearInput,
+             crime_type == input$crimeInput) %>% 
+      mutate(rank = dense_rank(desc(Numbers)))
+      
+    
+    leaflet(data = leaf_data, options = leafletOptions(minZoom = 4)) %>%
+      addProviderTiles(providers$Hydda.Base) %>%
+      addProviderTiles(providers$Stamen.TonerLite, options = providerTileOptions(opacity = 0.35)) %>%
+      setView(lng = -93.82,
+              lat = 37.44,
+              zoom = 4) %>%
+      addCircleMarkers(
+        ~ long,
+        ~ lat,
+        radius =  ~ (Numbers * 30 / max(Numbers)),
+        color = 'red',
+        weight = 3,
+        popup = ~ paste(
+          city,
+          "</br><b>Rank:</b>",
+          rank,
+          "</br><b>Crime:</b>",
+          crime_type,
+          "</br><b>Year:</b>",
+          year,
+          "</br><b>Total:</b>",
+          round(Numbers)
+        ),
+        fillOpacity = 0.5
+      )
+  })
+  
+  output$panelPlot <- renderPlot({
+    data %>% filter(crime_type == input$crimeInput, year == input$yearInput) %>%
+      arrange(desc(Numbers)) %>%
+      slice(1:5) %>%
+      ggplot(aes(
+        y = Numbers,
+        x = reorder(city, Numbers),
+        fill = "red"
+      )) +
+      geom_bar(stat = "identity") +
+      coord_flip() +
+      xlab("Violent Crime Numbers") +
+      theme_classic() +
+      theme(
+        axis.text.y = element_text(
+          color = "grey20",
+          size = 12,
+          angle = 0,
+          hjust = 1,
+          vjust = 0,
+          face = "plain"
+        ),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        legend.position = "none",
+        axis.line.x = element_blank(),
+        axis.line.y = element_blank()
+      )
+    
+  }, height = 150, width = "auto")
+  
+  output$trendPlot <- renderPlotly({
+    p <- data %>%
+      arrange(year) %>%
+      group_by(crime_type) %>%
+      filter(city == input$cityInput,
+             year >= (2015 - max(as.numeric(
+               input$durationInput
+             ))),
+             crime_type == input$crimeTrendInput) %>%
+      ggplot(aes(x = year, y = Rates, color = city)) +
+      geom_path() +
+      scale_colour_brewer(palette = "Set1") +
+      labs(
+        title = 'Comparison of Violent Crime Rates in Selected Cities',
+        subtitle = '',
+        y = 'Crime per 100k Population',
+        legend = 'Cities'
+      ) +
+      theme(
+        title = element_text(size = 12),
+        legend.title = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 16),
+        axis.text.y = element_text(size = 16),
+        legend.text = element_text(size = 10)
+      )
+    ggplotly(p, tooltip = c('label', 'x', 'y', 'colour'))
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
--- milestone3_writeup.md
+++ milestone3_writeup.md
@@ -9,3 +9,7 @@ In the **fly-on-the-wall** session, we noticed that the title _America’s Safes
 ### Tab 2: Line graph comparing crime rate between cities
 
 Moving to the second tab, users had to turn to the filters because the line chart didn’t display crime trends for any cities by default. This was noted as a feature to be enhanced. After selecting some cities in the drop-down selections and watching the line chart come to life, users got the message that the visualization intended to convey right away. There were mixed feelings about the flexibility to select trend duration (number of years before 2015). One point of view is that the missing data necessitates the full forty-year trend otherwise the trend doesn’t render for many cities. The other point of view is to maintain the feature and improve it with augmented data.
+
+### Summary
+
+We've summarized a list of proposed enhancements to the app. As we iteratively work through changes, we will be updating the checklist [here](https://github.com/UBC-MDS/americas-safest-cities/issues/14).
